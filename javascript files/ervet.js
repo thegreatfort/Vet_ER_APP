41aa78799caf9ca69dd578bfee58b5bf3327d958
thegreatfort <dgarrison247@gmail.com>
2018-12-18 22:04:53 -0600

    Final structure push

--- javascript files/ervet.js
+++ javascript files/ervet.js
@@ -1,19 +1,25 @@
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
-function dropping() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+// const words = document.getElementsByTagName("A");
+const list = document.getElementsByClassName("child");
+const menuz = document.getElementById("myDropdown");
+const button = document.getElementById('drop');
+
+// console.log(list.textContent);
+// console.log(menuz.children);
+console.log(button.textContent);
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+
+  Array.from(list).forEach(function(numbers){
+    const title = numbers.textContent;
+    console.log(title);
+    
+    numbers.addEventListener('click', function(e){
+      if(e.target.className == "child"){
+        button.innerHTML = title;
       }
-    }
-  }
+    })
+  })
+}
